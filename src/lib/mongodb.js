1cd5fecd7dfcda460ed2e802fcac4b5871a3f7d4
Sayan Biswas <sayanbiswas6073@gmail.com>
2024-11-12 22:13:09 +0530

    bug fixing

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -7,6 +7,7 @@ const connectDB = async() => {
     
     if (!global._mongoClientPromise) {
       client = new MongoClient(process.env.MONGODB_URI);
+      console.log(client)
       global._mongoClientPromise = client.connect();
     }
     clientPromise = global._mongoClientPromise;
