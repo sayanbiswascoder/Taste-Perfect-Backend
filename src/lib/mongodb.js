4e7e4e7efd8e0e76134ee03ee6bbe72c74bedb14
Sayan Biswas <sayanbiswas6073@gmail.com>
2024-11-12 21:43:02 +0530

    first commit

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -6,10 +6,7 @@ const connectDB = async() => {
     let clientPromise;
     
     if (!global._mongoClientPromise) {
-      client = new MongoClient(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+      client = new MongoClient(process.env.MONGODB_URI);
       global._mongoClientPromise = client.connect();
     }
     clientPromise = global._mongoClientPromise;
