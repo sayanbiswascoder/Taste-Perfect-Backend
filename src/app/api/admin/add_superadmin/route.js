e9a7f5844727e7d5b5f72509f06b6d0a01c80009
Sayan Biswas <sayanbiswas6073@gmail.com>
2024-11-12 22:05:44 +0530

    first commit

--- src/app/api/admin/add_superadmin/route.js
+++ src/app/api/admin/add_superadmin/route.js
@@ -7,6 +7,8 @@ import sendEmail from "@/utils/sendEmail"
 const GET = async(req, res) => {
     const db = await connectDB()
     const password = generateTempPassword()
+
+    console.log("DB", db)
         
     db.collection('admins').insertOne({
         name: 'Sayan Biswas',
@@ -16,9 +18,13 @@ const GET = async(req, res) => {
         superadmin: true,
     })
 
+    console.log("database entry")
+
     sendEmail('sayanbiswas6073@gmail.com', 'Welcome to Taste Perfect', `
         <h1></h1>Your Temporary Password is: ${password}</h1>
     `);
+
+      console.log("Email sent")
 
     return NextResponse.json({ "success": "true" }, { status: 200 });
 }
