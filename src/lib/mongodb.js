c454ae16d9503611c7b52c835cc055ebd05defbf
Sayan Biswas <sayanbiswas6073@gmail.com>
2024-11-12 22:40:34 +0530

    bug fixing

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,25 +3,26 @@ import mongoose from 'mongoose';
 
 
 const connectDB = handler => async(req, res) => {
-    // let client;
-    // let clientPromise;
+    let client;
+    let clientPromise;
     
-    // if (!global._mongoClientPromise) {
-    //   client = new MongoClient(process.env.MONGODB_URI);
-    //   global._mongoClientPromise = client.connect();
-    // }
-    // clientPromise = global._mongoClientPromise;
-    // client = await clientPromise;
-    // console.log(client)
-
-    // return client.db('taste-perfect');
-    const uri = process.env.MONGODB_URI;
-    if(mongoose.connections[0].readyState){
-        return handler(req,res)
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(process.env.MONGODB_URI);
+      global._mongoClientPromise = await client.connect();
     }
-    await mongoose.connect(uri)
+    console.log(global._mongoClientPromise)
+    clientPromise = global._mongoClientPromise;
+    client = await clientPromise;
+    console.log(client)
+
+    return client.db('taste-perfect');
+    // const uri = process.env.MONGODB_URI;
+    // if(mongoose.connections[0].readyState){
+    //     return handler(req,res)
+    // }
+    // await mongoose.connect(uri)
     
-    return handler(req,res)
+    // return handler(req,res)
 }
 
 export default connectDB;
