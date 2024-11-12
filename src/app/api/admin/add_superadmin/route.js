fb2067a89b3caf7f5f86940e6d36ee9fae633ba0
Sayan Biswas <sayanbiswas6073@gmail.com>
2024-11-12 21:32:02 +0530

    first commit

---
+++ src/app/api/admin/add_superadmin/route.js
@@ -0,0 +1,26 @@
+import { hashPassword } from "@/lib/auth"
+import { generateTempPassword } from "@/utils/generateTempPass"
+
+const { default: connectDB } = require("@/lib/mongodb")
+const { NextResponse } = require("next/server")
+
+const GET = async(req, res) => {
+    const db = await connectDB()
+    const password = generateTempPassword()
+        
+    db.collection('admins').insertOne({
+        name: 'Sayan Biswas',
+        email: 'sayanbiswas6073@gmail.com',
+        mobile: 9932981720,
+        password: await hashPassword(password),
+        superadmin,
+    })
+
+    sendEmail(email, 'Welcome to Taste Perfect', `
+        <h1></h1>Your Temporary Password is: ${password}</h1>
+    `);
+
+    return NextResponse.json({}, { status: 200 });
+}
+
+export { GET}
